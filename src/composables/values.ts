17bf8ffcbf777a0a3c67108ea3388740e0b06d03
ImperiusSha <svenhott@gmail.com>
2023-11-24 13:32:37 +0100

    chore: removed unnecessary class

--- src/composables/values.ts
+++
@@ -1,5 +0,0 @@
-import { ref } from 'vue';
-
-export const co2Value = ref<number | null>(null);
-export const pm10Value = ref<number | null>(null);
-export const tempValue = ref<number | null> (null);
