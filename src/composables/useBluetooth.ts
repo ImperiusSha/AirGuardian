4c8dd8316fe96b9d9c2f4e0df1235efa7cfbb6f9
ImperiusSha <svenhott@gmail.com>
2024-02-05 21:19:51 +0100

    feat: implemented logic to handle web-connection via BLE once it is supported

--- src/composables/useBluetooth.ts
+++ src/composables/useBluetooth.ts
@@ -1,6 +1,11 @@
-import { BleClient, BleDevice, BleService } from '@capacitor-community/bluetooth-le';
-import { nextTick, onMounted, ref } from 'vue';
+import { BleClient, BleDevice } from '@capacitor-community/bluetooth-le';
+import { onMounted, ref } from 'vue';
 import { useStore } from 'vuex';
+
+interface BluetoothDevice {
+    deviceId: string;
+    name?: string;
+}
 
 const CHARACTERISTICS = [
     '61fac1c0-75c6-4216-a801-9241dea5eac6', //CO2
@@ -19,12 +24,31 @@ export function useBluetooth() {
     const store = useStore();
     const isBluetoothAvailable = ref(false);
     const connectedDeviceId = ref('');
+    const selectedDevice = ref<BluetoothDevice | null>(null);
 
-    onMounted(() => {
+    const isMobileDevice = () => {
+        const userAgent = navigator.userAgent || navigator.vendor;
+        return /android|iphone|ipad|ipod|windows phone/i.test(userAgent);
+    };
+
+    const isFirefoxBrowser = () => {
+        return /firefox/i.test(navigator.userAgent);
+    };
+
+    const scanForDevices = async () => {
+        if (isMobileDevice()) {
+            await scanForDevicesMobile();
+        } else if (!isFirefoxBrowser()) {
+            await scanForDevicesBrowser();
+        }
+    };
+
+    onMounted(async () => {
         isBluetoothAvailable.value = 'bluetooth' in navigator && typeof (navigator as any).bluetooth.requestDevice === 'function';
     });
 
-    const scanForDevices = async () => {
+    // Funktion für das Scannen auf mobilen Geräten
+    const scanForDevicesMobile = async () => {
         availableDevices.value = [];
         try {
             await BleClient.initialize();
@@ -41,12 +65,34 @@ export function useBluetooth() {
         }
     };
 
+    // Funktion für das Scannen in Browsern (außer Firefox)
+    const scanForDevicesBrowser = async () => {
+        availableDevices.value = [];
+        try {
+            const device = await (navigator as any).bluetooth.requestDevice({
+                acceptAllDevices: true,
+                optionalServices: ['3f55130b-0ef5-4d81-b9d4-618a5ccbf2ac'] // Muss aktuell noch fest definiert werden
+            });
+            selectedDevice.value = {
+                deviceId: device.id,
+                name: device.name
+            };
+            availableDevices.value.push(device);
+            console.log('Gefundenes Gerät:', device);
+        } catch (error) {
+            console.error('Fehler beim Scannen (Browser):', error);
+        }
+    };
+
+
     const MAX_ATTEMPTS = 3;
     const RETRY_INTERVAL = 5000;
 
+    // Funktion zur Überprüfung des Verbindungsstatus
     const checkConnectionStatus = async () => {
         let attempts = 0;
-    
+
+        // Versucht 3x, die Verbindung wiederherzustellen
         while (attempts < MAX_ATTEMPTS && !isConnected.value) {
             try {
                 await BleClient.readRssi(connectedDeviceId.value);
@@ -66,50 +112,119 @@ export function useBluetooth() {
         }
     };
 
-    const connectToSensor = async (device: { deviceId: string; }) => {
+    // Funktion zum Verbinden mit dem Gerät für Web
+    const connectToDeviceWeb = async () => {
         try {
-            if (connectedDeviceId.value && connectedDeviceId.value !== device.deviceId) {
-                await disconnectFromSensor();
-            }
-            await BleClient.stopLEScan();
-            await BleClient.connect(device.deviceId);
+            console.log('Anfrage nach einem Bluetooth-Gerät...');
+            const device = await (navigator as any).bluetooth.requestDevice({
+                acceptAllDevices: true,
+                optionalServices: ['3f55130b-0ef5-4d81-b9d4-618a5ccbf2ac'] // Die Service UUID, muss aktuell fest definiert werden für Web
+            });
 
-            // Abholen der vorhandenen Services
-            const servicesFromDevice = await BleClient.getServices(device.deviceId);
+            console.log('Verbindung zum Gerät herstellen...');
+            const server = await device.gatt.connect();
+            selectedDevice.value = { deviceId: device.id, name: device.name };
+            isConnected.value = true;
+            connectedDeviceId.value = device.id;
+            // store.commit('showNotification', 'Verbindung erfolgreich hergestellt.');
 
-            // Nimmt den ersten Service, der nicht zu den generischen gehört ('0000')
-            const customService = servicesFromDevice.find(service => !service.uuid.startsWith('0000'));
-            if (!customService) {
-                throw new Error('Kein Custom Service gefunden.');
-            }
+            console.log('Hole alle Services vom Gerät...');
+            const services = await server.getPrimaryServices();
 
-            // Abonniert die Benachrichtigungen für jede bekannte Charakteristik
-            for (const charUuid of CHARACTERISTICS) {
-                try {
-                    await BleClient.startNotifications(device.deviceId, customService.uuid, charUuid, (notifValue) => {
-                        const value = decoder.decode(notifValue.buffer);
-                        const mutation = getMutationForCharacteristic(charUuid);
-                        if (mutation) {
-                            store.commit(mutation, parseFloat(value));
+            for (const service of services) {
+                console.log(`Arbeite mit Custom Service: ${service.uuid}`);
+                const characteristics = await service.getCharacteristics();
+
+                for (const characteristic of characteristics) {
+                    if (CHARACTERISTICS.map(c => c.toLowerCase()).includes(characteristic.uuid)) {
+                        if (characteristic.properties.notify) {
+                            try {
+                                await characteristic.startNotifications();
+                                characteristic.addEventListener('characteristicvaluechanged', (event: { target: { value: { buffer: AllowSharedBufferSource | undefined; }; }; }) => {
+                                    const value = decoder.decode(event.target.value.buffer);
+                                    const mutation = getMutationForCharacteristic(characteristic.uuid);
+                                    if (mutation) {
+                                        console.log(`Empfangener Wert für ${characteristic.uuid}:`, value);
+                                        store.commit(mutation, parseFloat(value));
+                                    } else {
+                                        console.warn(`Keine Vuex-Mutation zugeordnet für die Charakteristik: ${characteristic.uuid}`);
+                                    }
+                                });
+                                console.log(`Benachrichtigungen für ${characteristic.uuid} gestartet.`);
+                            } catch (error) {
+                                console.error(`Fehler beim Starten der Benachrichtigungen für Charakteristik ${characteristic.uuid}:`, error);
+                            }
                         } else {
-                            console.warn(`Keine Vuex-Mutation zugeordnet für die Charakteristik: ${charUuid}`);
+                            console.log(`Charakteristik ${characteristic.uuid} unterstützt keine Benachrichtigungen.`);
                         }
-                    });
-                } catch (error) {
-                    console.error(`Fehler beim Abonnieren der Charakteristik ${charUuid}:`, error);
+                    }
                 }
             }
 
-            isConnected.value = true;
-            connectedDeviceId.value = device.deviceId;
-            store.commit('showNotification', 'Verbindung erfolgreich hergestellt.');
-
-            // Regelmäßige Überprüfung, ob die Verbindung noch besteht
-            setInterval(checkConnectionStatus, 30000); // Alle 30 Sekunden
         } catch (error) {
-            store.commit('showNotification', 'Verbindung fehlgeschlagen: ' + error);
+            console.error('Verbindung fehlgeschlagen:', error);
             isConnected.value = false;
             connectedDeviceId.value = '';
+            store.commit('showNotification', 'Verbindung fehlgeschlagen: ' + error);
+        }
+    };
+
+    const connectToSensor = async (device: BluetoothDevice) => {
+        if (isMobileDevice()) {
+            // Mobile Plattform: Verwenden des @capacitor-community/bluetooth-le Plugin
+            console.log('connectToSensor aufgerufen mit Gerät:', device.name);
+            try {
+                console.log('Stoppe BLE-Scan');
+                await BleClient.stopLEScan();
+
+                console.log('Verbinden mit Gerät: ' + device.deviceId);
+                await BleClient.connect(device.deviceId);
+
+                console.log('Hole Services vom Gerät:', device.deviceId);
+                const servicesFromDevice = await BleClient.getServices(device.deviceId);
+                console.log('Gefundene Services:', servicesFromDevice);
+
+                // Abfangen aller CustomServices, die nicht zu den Primary-Services gehören
+                const customService = servicesFromDevice.find(service => !service.uuid.startsWith('0000'));
+                if (!customService) {
+                    throw new Error('Kein Custom Service gefunden.');
+                }
+
+                // Abonnieren der Characteristics zum erhalt der Daten aus der Sensorbox
+                console.log('Abonniere Benachrichtigungen für Charakteristiken');
+                for (const charUuid of CHARACTERISTICS) {
+                    try {
+                        await BleClient.startNotifications(device.deviceId, customService.uuid, charUuid, (notifValue) => {
+                            const value = decoder.decode(notifValue.buffer);
+                            const mutation = getMutationForCharacteristic(charUuid);
+                            if (mutation) {
+                                console.log(`Empfangener Wert für ${charUuid}:`, value);
+                                store.commit(mutation, parseFloat(value));
+                            } else {
+                                console.warn(`Keine Vuex-Mutation zugeordnet für die Charakteristik: ${charUuid}`);
+                            }
+                        });
+                    } catch (error) {
+                        console.error(`Fehler beim Abonnieren der Charakteristik ${charUuid}:`, error);
+                    }
+                }
+
+                console.log('Verbindung zum Gerät erfolgreich hergestellt:', device.deviceId);
+                isConnected.value = true;
+                connectedDeviceId.value = device.deviceId;
+                store.commit('showNotification', 'Verbindung erfolgreich hergestellt.');
+
+                console.log('Setze regelmäßige Überprüfung der Verbindung');
+                setInterval(checkConnectionStatus, 30000);
+            } catch (error) {
+                console.error('Verbindung fehlgeschlagen:', error);
+                store.commit('showNotification', 'Verbindung fehlgeschlagen: ' + error);
+                isConnected.value = false;
+                connectedDeviceId.value = '';
+            }
+        } else {
+            // Web Plattform: Verwendet die Web Bluetooth API
+            await connectToDeviceWeb();
         }
     };
 
@@ -125,7 +240,6 @@ export function useBluetooth() {
         } catch (error) {
             store.commit('showNotification', 'Fehler beim Trennen der Verbindung: ' + error);
         }
-
     };
 
     return { isConnected, connectToSensor, disconnectFromSensor, isBluetoothAvailable, availableDevices, scanForDevices, connectedDeviceId };
